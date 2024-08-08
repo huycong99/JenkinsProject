dedee794f029d9dce5397fc2b2793b018d7b12a5
Your Name <you@example.com>
2024-08-08 20:04:11 +0700

    FirstCommit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using Serilog;
+
+namespace JenkinsProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var logger = new LoggerConfiguration();
+            logger.WriteTo.Console().CreateLogger();
+            
+
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
